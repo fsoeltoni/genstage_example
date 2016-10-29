a88b6a60c848c5dec305b6ab767b86c3d8683d8e
Robert Grayson <bobdawg@Roberts-MacBook-Pro-2.local>
2016-10-29 12:51:27 -0400

    extract database interface to its own module

--- lib/genstage_example/task.ex
+++ lib/genstage_example/task.ex
@@ -1,36 +1,13 @@
 defmodule GenstageExample.Task do
-  import Ecto.Query
-
-  alias GenstageExample.Repo
-
-  def update_status(id, status) do
-    Repo.update_all by_ids([id]), set: [status: status]
+  def enqueue(status, payload) do
+    GenstageExample.TaskDBInterface.insert_tasks(status, payload)
   end
 
-  def enqueue(status, payload) do
-    Repo.insert_all "tasks", [
-      %{status: status, payload: payload}
-    ]
+  def update_status(id, status) do
+    GenstageExample.TaskDBInterface.update_task_status(id, status)
   end
 
   def take(limit) do
-    {:ok, {count, events}} =
-      Repo.transaction fn ->
-        ids = Repo.all waiting(limit)
-        Repo.update_all by_ids(ids), [set: [status: "running"]], [returning: [:id, :payload]]
-      end
-    {count, events}
-  end
-
-  defp by_ids(ids) do
-    from t in "tasks", where: t.id in ^ids
-  end
-
-  defp waiting(limit) do
-    from t in "tasks",
-      where: t.status == "waiting",
-      limit: ^limit,
-      select: t.id,
-      lock: "FOR UPDATE SKIP LOCKED"
+    GenstageExample.TaskDBInterface.take_tasks(limit)
   end
 end
